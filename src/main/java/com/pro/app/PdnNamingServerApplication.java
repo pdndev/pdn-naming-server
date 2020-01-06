dbd5fe8cb2fa02731d7f43e682f930aff9fd681a
Arunkumar Arjunan <arunkumar.a@prodapt.com>
2020-01-06 09:00:45 +0530

    added

---
+++ src/main/java/com/pro/app/PdnNamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.pro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PdnNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdnNamingServerApplication.class, args);
+	}
+
+}
